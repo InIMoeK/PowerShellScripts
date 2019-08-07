6f35b7d86619fc1b45f37e09de5a21afb4170018
Robert Groenewald <inimoek@gmail.com>
2019-08-07 21:43:57 +0200

    Uncommented some parts

--- WAC installer/wacinstall.ps1
+++ WAC installer/wacinstall.ps1
@@ -10,24 +10,24 @@ $remoteip = ($defaultremoteip,$remoteip)[[bool]$remoteip]
 
 # Static variables
 
-#$url = 'http://aka.ms/WACDownload'
-#$dlpath = "C:\Users\$env:UserName\Downloads\WAC.msi"
-#$start_time = Get-Date
+$url = 'http://aka.ms/WACDownload'
+$dlpath = "C:\Users\$env:UserName\Downloads\WAC.msi"
+$start_time = Get-Date
 
 # Download Windows Admin Center installer
 
-#wget $url -OutFile $dlpath ;
+wget $url -OutFile $dlpath ;
 
-#Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" ;
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" ;
 
-#Write-Host "Installing Windows Admins Center" -ForegroundColor Green ;
+Write-Host "Installing Windows Admins Center" -ForegroundColor Green ;
 
 # Execute installer
 
-#msiexec /i $dlpath /qn /L*v log.txt SME_PORT=$serverport SSL_CERTIFICATE_OPTION=generate
+msiexec /i $dlpath /qn /L*v log.txt SME_PORT=$serverport SSL_CERTIFICATE_OPTION=generate
 
 # Add firewall rule
 
-#New-NetFirewallRule -DisplayName 'Windows Admin Center (HTTPS-In)' -Name 'Windows Admin Center (HTTPS-In)' -Profile Any -LocalPort $serverport -Protocol TCP -RemoteAddress $remoteip ;
+New-NetFirewallRule -DisplayName 'Windows Admin Center (HTTPS-In)' -Name 'Windows Admin Center (HTTPS-In)' -Profile Any -LocalPort $serverport -Protocol TCP -RemoteAddress $remoteip ;
 
 Write-Host "Firewall rule created" -ForegroundColor Green
